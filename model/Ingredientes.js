2cfbb5e1fb7b8cb96b83b86877b3f2ec0e6f73dc
ÉRICK MIGUEL SCHUCK <erick_schuck@estudante.sesisenai.org.br>
2025-03-14 08:17:35 -0300

    valor estimado parte 1

---
+++ model/Ingredientes.js
@@ -0,0 +1,33 @@
+export default class Ingrediente{
+    constructor(peso){
+        this.peso = peso
+    }
+
+    CaucularValorEstimado(quad){
+        let valor = [["Farinha de Trigo:", 13.50 / 1000],
+                     ["Fermento biológico seco:", 1.79 / 10],
+                     ["Leite morno desnatado:",6.49 / 1000],
+                     ["Ovos:",12.90 / 12],
+                     ["Manteiga sem sal:",16.29 / 200],
+                     ["Adoçante culinário:",38.50 / 100],
+                     ["Essência de baunilha:",11.90 / 100],
+                     ["Frutas cristalizadas:",19.90 / 1000],
+                     ["Uvas-passas:",27.90 / 1000],
+                     ["Sal:",3.89 / 1000]]
+
+let Quantidade = [["Farinha de trigo:",240],
+                  ["Fermento biológico seco:",5],
+                  ["Leite morno desnatado:",60],
+                  ["Ovos:",2],
+                  ["Manteiga sem sal:",35],
+                  ["Adoçante culinário:",10],
+                  ["Essência de baunilha:",5],
+                  ["Frutas cristalizadas:",80],
+                  ["Uvas-passas:",30],
+                  ["Sal:",1.5]]
+
+    for(let i=0;i<10;i++){
+        return 
+    }
+    }
+}
